6945488fac6b9277391c7230b9dabace81257ef3
Scott Bouloutian <scott@bouloutian.com>
2026-08-16 11:27:54 -0400

    Initial Commit

---
+++ spin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spin
+//
+//  Created by Scott Bouloutian on 8/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spin/spinApp.swift
@@ -0,0 +1,17 @@
+//
+//  spinApp.swift
+//  spin
+//
+//  Created by Scott Bouloutian on 8/16/26.
+//
+
+import SwiftUI
+
+@main
+struct spinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
